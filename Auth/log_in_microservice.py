5d04934554dc2a50c5cf358a46ebc8f73fef85bc
Yorbre25 <yraulbr25@gmail.com>
2024-06-04 12:39:50 -0600

    admin_rol

--- Auth/log_in_microservice.py
+++ Auth/log_in_microservice.py
@@ -1,28 +1,53 @@
 from flask import Flask, request, jsonify
-from firebase_credentials import firebase_config
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 import pyrebase
 import requests
 
+from firebase_credentials import firebase_config
+
 app = Flask(__name__)
 
-
+# Init Firebase SDK to create user
 firebase = pyrebase.initialize_app(firebase_config)
 auth = firebase.auth()
+
+# Init Firebase Admin SDK to user firestore
+cred = credentials.Certificate('Auth/firebase_admin_sdk_credentials.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 def handle_error(e):
     if isinstance(e, requests.exceptions.HTTPError):
         return jsonify({"Error": "Invalid Login Credentials"}), 400
     else:
         return jsonify({"Error": str(e), "Type": str(type(e))}), 500
-    
 
+def log_in_with_firebase(email, password):
+    return auth.sign_in_with_email_and_password(email=email, password=password)
+
+def get_user_data(email):
+    doc_ref = db.collection('users').document(email)
+    doc = doc_ref.get()
+    return doc.to_dict()
+
+def append_user_data(user, email):
+    data = get_user_data(email)
+    user["isAdmin"] = data["isAdmin"]
+    user["displayName"] = data["name"]
+    return user
+    
+    
 @app.route('/log_in', methods=['POST'])
 def log_in():
-    data = request.json
-    email = data['email']
-    password = data['password']
     try: 
-        user = auth.sign_in_with_email_and_password(email=email, password=password)
+        data = request.json
+        email = data['email']
+        password = data['password']
+        
+        user = log_in_with_firebase(email, password)
+        user = append_user_data(user, email)
         return jsonify(user), 200
     except Exception as e:
         return handle_error(e)
--- Auth/sign_up_microservice.py
+++ Auth/sign_up_microservice.py
@@ -1,12 +1,21 @@
 from flask import Flask, request, jsonify
 from firebase_credentials import firebase_config
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 import pyrebase
 import requests
 
 app = Flask(__name__)
 
+# Init Firebase SDK to create user
 firebase = pyrebase.initialize_app(firebase_config)
 auth = firebase.auth()
+
+# Init Firebase Admin SDK to user firestore
+cred = credentials.Certificate('Auth/firebase_admin_sdk_credentials.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 # Todo: Check if user already exists
 def handle_error(e):
@@ -16,13 +25,25 @@ def handle_error(e):
         return jsonify({"Error": str(e), "Type": str(type(e))}), 500
     
 
+def add_data(email, name):
+    doc_ref = db.collection('users').document(email)
+    doc_ref.set({
+        'name': name,
+        'isAdmin': False
+    })
+
+def sign_up_with_firebase(email, password):
+    return auth.create_user_with_email_and_password(email=email, password=password)
+
 @app.route('/sign_up', methods=['POST'])
 def sign_up():
-    data = request.json
-    email = data['email']
-    password = data['password']
     try: 
-        user = auth.create_user_with_email_and_password(email=email, password=password)
+        data = request.json
+        name = data['name']
+        email = data['email']
+        password = data['password']
+        user = sign_up_with_firebase(email, password)
+        add_data(email, name)
         return jsonify(user), 200
     except Exception as e:
         return handle_error(e)
