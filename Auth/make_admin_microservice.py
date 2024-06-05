bdecffc9dc4c1b70908deac662229aec89756836
Yorbre25 <yraulbr25@gmail.com>
2024-06-05 14:36:15 -0600

    add: only admin can upgrate to admin

--- Auth/make_admin_microservice.py
+++ Auth/make_admin_microservice.py
@@ -6,33 +6,62 @@ from firebase_admin import firestore
 
 app = Flask(__name__)
 
-# todo: Check for admin attribute in jwt
 
 cred = credentials.Certificate('Auth/firebase_admin_sdk_credentials.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-def handle_error(e):
-    if isinstance(e, firebase_admin._auth_utils.UserNotFoundError):
-        return jsonify({"Error": "User not found"}), 400
-    else:
-        return jsonify({"Error": str(e), "Type": str(type(e))}), 500
-
-def make_admin_with_firebase(user_id):
-    custom_claim = {
-        'isAdmin': True
-    }
-    auth.set_custom_user_claims(user_id, custom_claim)
-    
 @app.route('/make_admin', methods=['POST'])
 def make_admin():
     try:
-        data = request.json
-        user_id = data['user_id']
-        make_admin_with_firebase(user_id)
-        return jsonify({"Success": "User is now an admin"}), 200
+        token = request.headers['Authorization']
+        if (is_request_valid(token)):
+            data = request.json
+            email = data['email']
+            make_admin_with_firebase(email)
+            return jsonify({"Success": "User is now an admin"}), 200
+        else:
+            raise Exception("Unauthorized")
     except Exception as e:
         return handle_error(e)
+
+def is_request_valid(token):
+    decoded_token = validate_and_decode_token(token)
+    return is_admin(decoded_token)
+
+def validate_and_decode_token(token):
+    try:
+        decoded_token = auth.verify_id_token(token)
+        return decoded_token
+    except:
+        raise Exception("Invalid token")
+
+def is_admin(decoded_token):
+    print(decoded_token)
+    isAdmin = decoded_token["isAdmin"]
+    if isAdmin:
+        return True
+    else:
+        raise Exception("Unauthorized")
+    
+def make_admin_with_firebase(email):
+    user = auth.get_user_by_email(email)
+    print(type(user))
+    custom_claim = {
+        'isAdmin': True
+    }
+    auth.set_custom_user_claims(user.uid, custom_claim)
+    
+def handle_error(e):
+    # if isinstance(e, firebase_admin._auth_utils.UserNotFoundError):
+    #     return jsonify({"Error": "User not found"}), 400
+    # elif str(e) == "Invalid token":
+    #     return jsonify({"Error": "Invalid Token"}), 401
+    # elif str(e) == "Unauthorized":
+    #     return jsonify({"Error": "Unauthorized"}), 401
+    # else:
+        return jsonify({"Error": str(e), "Type": str(type(e))}), 500    
+
 
 
 if __name__ == '__main__':
--- Auth/sign_up_microservice.py
+++ Auth/sign_up_microservice.py
@@ -18,20 +18,18 @@ cred = credentials.Certificate('Auth/firebase_admin_sdk_credentials.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-# Todo: Check if user already exists
-def handle_error(e):
-    if isinstance(e, requests.HTTPError):
-        return {"Error": "Invalid Login Credentials"}, 400
-    else:
-        return jsonify({"Error": str(e), "Type": str(type(e))}), 500
-    
-
-def add_data(email, name):
-    doc_ref = db.collection('users').document(email)
-    doc_ref.set({
-        'name': name,
-        "email": email,
-    })
+@app.route('/sign_up', methods=['POST'])
+def sign_up():
+    try: 
+        data = request.json
+        name = data['name']
+        email = data['email']
+        password = data['password']
+        user = sign_up_with_firebase(email, password)
+        add_data(email, name, user['localId'])
+        return jsonify(user), 200
+    except Exception as e:
+        return handle_error(e)
 
 def sign_up_with_firebase(email, password):
     user = authentification.create_user_with_email_and_password(email=email, password=password)
@@ -41,19 +39,23 @@ def sign_up_with_firebase(email, password):
     auth.set_custom_user_claims(user["localId"], add_custom_claim)
     return authentification.sign_in_with_email_and_password(email=email, password=password)
 
-@app.route('/sign_up', methods=['POST'])
-def sign_up():
-    try: 
-        data = request.json
-        name = data['name']
-        email = data['email']
-        password = data['password']
-        user = sign_up_with_firebase(email, password)
-        add_data(email, name)
-        return jsonify(user), 200
-    except Exception as e:
-        return handle_error(e)
-        
+def add_data(email, name, user_id):
+    doc_ref = db.collection('users').document(email)
+    doc_ref.set({
+        'name': name,
+        "email": email,
+        "user_id": user_id
+    })
+
+def handle_error(e):
+    if isinstance(e, requests.HTTPError):
+        return {"Error": "Invalid Login Credentials"}, 400
+    else:
+        return jsonify({"Error": str(e), "Type": str(type(e))}), 500
+    
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
